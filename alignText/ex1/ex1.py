4429de481961fbc0b227645b6b1b376e61e5b85c
codeOverFlow <adrien.bodineau@gmail.com>
2015-09-08 01:41:26 +0200

    finish preprocessing and start finding

--- alignText/ex1/ex1.py
+++ alignText/ex1/ex1.py
@@ -3,24 +3,78 @@
 # author: Adrien Bodineau
 
 import re
+import codecs
+import unicodedata
 
-filter_tag = re.compile("DET|DTN|PREP|TO|DT|\W")
+files_source = {}
+files_target = {}
 
-with open('termer_source/corpus.lem', 'r') as source:
-    with open('termer_target/corpus.lem', 'r') as target:
+def get_words(file_to_read, dictionary):
+    with codecs.open(file_to_read, 'r', 'utf-8') as source:
+        filter_tag = re.compile("[A-Z]")
+
         line_source = source.read().split(' ')
-        line_target = target.read().split(' ')
+        
+        file_name = ''
+        for l in line_source:
 
-        for l, ll in zip(line_source, line_target):
-            if l != '' and not filter_tag.match(l):
-                print not filter_tag.match(l)
-                lem = l.split('/')[-1]
-                if ':' in lem:
-                    lem = lem.split(':')[0]
-                print l
-                print lem, '\n\n'
-            #if ll != '' and not filter_tag.match(ll):
-            #    lem = ll.split('/')[-2]
-            #    print ll
-            #    print lem, '\n\n'
+            #print file_name
+
+            # extract filename
+            if '__FILE' in l:
+                file_name = l.split('/')[0]
+                file_name = file_name.split('=')[1]
+                dictionary[file_name] = []
+                continue
+            elif '__ENDFILE' in l:
+                continue
+
+            if len(l) > 1:
+                l = l.strip()
+                #print l
+                l = l.split('/')
+                #print l
+        
+                # get the word and remove accents
+                word = l[0]
+                word = unicodedata.normalize('NFD', word).encode('ascii', 'ignore')
+        
+                # get the tag
+                tag = l[1]
+                if not filter_tag.match(tag):
+                    #print '\n\n'
+                    continue
+                elif ':' in tag:
+                    tag = tag.split(':')[0]
+
+                dictionary[file_name].append((word, tag))
+                #print files_source
+                #print word, tag, '\n\n'
+
+
+def find_transfuges(dict_source, dict_target):
+    word_list_source = []
+    word_list_target = []
+
+    for key in dict_source.keys():
+        word_list_source = [w for w,l in dict_source[str(key)]]
+    for key in dict_target.keys():
+        word_list_target = [w for w,l in dict_target[str(key)]]
+    print set(word_list_source).intersection(word_list_target)
+
+print '###############################################################'
+print '################## START PREPROCESSING      ###################'
+print '################## START SOURCE             ###################'
+get_words('termer_source/corpus.lem', files_source)
+print '################## END   SOURCE             ###################'
+print '################## START TARGET             ###################'
+get_words('termer_target/corpus.lem', files_target)
+print '################## END   TARGET             ###################'
+print '################## END   PREPROCESSING      ###################'
+print '###############################################################', '\n\n'
+
+print '################## SEARCH TRANSFUGES        ###################', '\n'
+find_transfuges(files_source, files_target)
+print '\n', '###############################################################'
+print '################## SEARCH COGNAT CANDIDATES ###################'
 
